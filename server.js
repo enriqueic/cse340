358c84a77d34c2435a8c0c097e1ac53de71897d3
enriqueic <eibarracuanalo@byupathway.edu>
2026-04-14 02:19:02 -0700

    W03 learning 3/3

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
+const Util = require("./utilities")
 
 /* ***********************
  * View Engine and Templates
@@ -24,8 +25,26 @@ app.set("layout", "./layouts/layout")
  *************************/
 app.use(static)
 // Index route
-app.get("/", baseController.buildHome)
+app.get("/", Util.handleErrors(baseController.buildHome))
 app.use("/inv", require("./routes/inventoryRoute"))
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await Util.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
